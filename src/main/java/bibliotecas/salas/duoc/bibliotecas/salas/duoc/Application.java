3583d21acbff0175f285b322a61a8a84ccc7a1e4
Cetecom <32@yahoo>
2025-05-20 16:40:52 -0400

    agregando

---
+++ src/main/java/bibliotecas/salas/duoc/bibliotecas/salas/duoc/Application.java
@@ -0,0 +1,13 @@
+package bibliotecas.salas.duoc.bibliotecas.salas.duoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/bibliotecas/salas/duoc/bibliotecas/salas/duoc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package bibliotecas.salas.duoc.bibliotecas.salas.duoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
